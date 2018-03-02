7c4e2f4897185e5d80ccfc89b4d725c2c52b4767
Jay Wiggins <jayw@Jays-MacBook.local>
2018-03-02 09:17:01 -0700

    adding graphic files and quick test...

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,29 @@
 #include "FastLED.h"
-#define NUM_LEDS 8
+#define NUM_LEDS 56
+#define DATA_PIN 6
+
+uint8_t lightsPerSegment = 8;
+uint8_t a[] = { 0, 1, 2, 3, 4, 5, 6, 7 };
+uint8_t b[] = { 8, 9,10,11,12,13,14,15 };
+uint8_t hue = 0;
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
-  FastLED.addLeds<NEOPIXEL, 6>(leds, NUM_LEDS);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void loop() {
-  int color = 0xFF44DD;
-	leds[0] = color;
-	leds[1] = color;
-	leds[2] = color;
-	leds[3] = color;
-	leds[4] = color;
-	leds[5] = color;
-	leds[6] = color;
-	leds[7] = color;
-  FastLED.show();
-  delay(500);
-	leds[0] = CRGB::Black;
-	leds[1] = CRGB::Black;
-	leds[2] = CRGB::Black;
-	leds[3] = CRGB::Black;
-	leds[4] = CRGB::Black;
-	leds[5] = CRGB::Black;
-	leds[6] = CRGB::Black;
-	leds[7] = CRGB::Black;
+  hue += 20;
+  for(uint8_t x = 0; x < lightsPerSegment; x++) {
+    uint8_t currentLed = a[x];
+    leds[currentLed] = CHSV( hue, 255, 128);
+  }
+  hue += 20;
+  for(uint8_t x = 0; x < lightsPerSegment; x++) {
+    uint8_t currentLed = b[x];
+    leds[currentLed] = CHSV( hue, 255, 128);
+  }
   FastLED.show();
   delay(500);
 }
